a44678bee0c48986c383ebf69381a085c9c4c4ce
Jackson Wong <wongjt@berkeley.edu>
2024-09-28 16:20:37 -0700

    mobile menu fix

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,24 +4,11 @@ import { useState } from 'react'
 import {
   Dialog,
   DialogPanel,
-  Disclosure,
-  DisclosureButton,
-  DisclosurePanel,
-  Popover,
-  PopoverButton,
-  PopoverGroup,
-  PopoverPanel,
 } from '@headlessui/react'
 import {
-  ArrowPathIcon,
   Bars3Icon,
-  ChartPieIcon,
-  CursorArrowRaysIcon,
-  FingerPrintIcon,
-  SquaresPlusIcon,
   XMarkIcon,
 } from '@heroicons/react/24/outline'
-import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
 
 
 const NavItems = [
@@ -46,6 +33,21 @@ const NavItems = [
 export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
+  const maybeOpenMobileMenu = (
+    !mobileMenuOpen && (
+      <div className="flex lg:hidden">
+        <button
+          type="button"
+          onClick={() => setMobileMenuOpen(true)}
+          className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+        >
+          <span className="sr-only">Open main menu</span>
+          <Bars3Icon aria-hidden="true" className="h-6 w-6" />
+        </button>
+      </div>
+    )
+  );
+
   return (
     <header className="bg-white">
       <nav aria-label="Global" className="mx-auto flex max-w-screen items-center justify-between p-6 lg:px-8">
@@ -55,16 +57,7 @@ export default function Header() {
             <img alt="" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" className="h-8 w-auto" />
           </a>
         </div>
-        <div className="flex lg:hidden">
-          <button
-            type="button"
-            onClick={() => setMobileMenuOpen(true)}
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-          >
-            <span className="sr-only">Open main menu</span>
-            <Bars3Icon aria-hidden="true" className="h-6 w-6" />
-          </button>
-        </div>
+        {maybeOpenMobileMenu}
         <div className="hidden lg:flex lg:gap-x-12">
           {NavItems.map((item) => (
             <a href={item.href} key={item.name} className="text-sm font-semibold leading-6 text-gray-900">
@@ -83,7 +76,7 @@ export default function Header() {
         <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
+              <span className="sr-only">Jackson Wong</span>
               <img
                 alt=""
                 src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
